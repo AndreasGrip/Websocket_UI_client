3548432fb00d87049411cf1ab852ca27ab4e87ab
Andreas <andreas.grip@gmail.se>
2020-12-17 18:13:10 +0100

    To many changes

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-const { dialog, ipcMain, app, BrowserWindow } = require("electron");
+const { dialog, ipcMain, app, BrowserWindow } = require('electron');
+const fs = require('fs').promises;
+
 function createWindow() {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
@@ -11,40 +13,53 @@ function createWindow() {
 
   // and load the index.html of the app.
   // mainWindow.loadFile("index.html");
-  mainWindow.loadFile("index.html");
+  mainWindow.loadFile('index.html');
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools();
 }
 
-ipcMain.on("channelJoin", (event, arg) => {
-    console.log('Joined ' + arg);
-    ipcMain.on("channelClick" + arg, (event => {
-      console.log("user clicked on channel " + arg);
-    }))
-  })
-  
-  
-  
-  // This method will be called when Electron has finished
-  // initialization and is ready to create browser windows.
-  // Some APIs can only be used after this event occurs.
-  app.on("ready", createWindow);
-  
-  // Quit when all windows are closed, except on macOS. There, it's common
-  // for applications and their menu bar to stay active until the user quits
-  // explicitly with Cmd + Q.
-  app.on("window-all-closed", function () {
-    if (process.platform !== "darwin") {
-      app.quit();
-    }
+ipcMain.on('quit', (event, arg) => {
+  app.quit();
+});
+
+ipcMain.on('save', (event, arg) => {
+  console.log('arg: ' + arg);
+  fs.writeFile('.history', arg, 'utf8').then((data) => {
+    console.log('.history file saved.');
+    event.reply('app-event', '.history file saved.');
   });
-  
-  app.on("activate", function () {
-    // On OS X it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow();
-    }
-  });
-  
+});
+
+ipcMain.on('load', (event, arg) => {
+  fs.readFile('.history', 'utf8')
+    .then((data) => {
+      event.reply('sendLoadReply', data);
+      console.log(data);
+    })
+    .catch((err) => {
+      event.reply('app-event', 'failed to load file Error:' + err.message);
+    });
+});
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow);
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', function () {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
--- renderer.js
+++ renderer.js
@@ -1,1 +1,36 @@
-const {ipcRenderer} = require('electron');
+const { ipcRenderer } = require('electron');
+
+app.ipc.sendQuit = () => {
+  ipcRenderer.send('quit', '');
+};
+app.ipc.sendSave = () => {
+  const app = document.app;
+  let instances = [];
+  for (let i = 0; i < app.instances.length; i++) {
+    const element = app.instances[i];
+    let instance = {};
+    instance.id = element.id;
+    instance.history = element.commandlineImpretor.history;
+    instance.preHistory = element.commandlineImpretor.preHistory;
+    instances.push(instance);
+  }
+  let objectToSave = JSON.stringify(instances);
+  console.log(objectToSave);
+  ipcRenderer.send('save', objectToSave);
+};
+app.ipc.sendLoad = () => {
+  ipcRenderer.send('load');
+};
+ipcRenderer.on('sendLoadReply', (event, arg) => {
+  console.log(arg);
+  app.activeInstance.commandlineImpretor.terminalAdd(arg, 'pagecontents_app-event');
+});
+
+ipcRenderer.on('app-event', (event, arg) => {
+  console.log('sko');
+  app.activeInstance.commandlineImpretor.terminalAdd(arg, 'pagecontents_app-event');
+});
+
+ipcRenderer.on('asynchronous-reply', (event, arg) => {
+  console.log(arg); // prints "pong"
+});
